a013d10acecdb9ffb3e6101ab9933c03676f1514
dhslrl321 <dhslrl321@gmail.com>
2021-08-27 18:15:51 +0900

    commit

---
+++ backend/src/test/java/com/example/jenkinscicdtodo/JenkinsCicdTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicdtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
