a29f7aea06ccf96951a4c9d5600eae7c55b358d5
dhslrl321 <dhslrl321@gmail.com>
2021-08-30 22:35:18 +0900

    commit

--- frontend/src/util/SERVER.js
+++ frontend/src/util/SERVER.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const SERVER = axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: "http://3.38.95.48",
   headers: {
     "Content-Type": "application/json",
   }
